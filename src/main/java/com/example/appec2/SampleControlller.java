a6b723fd2a34f9bce3fcbec80e57eaab876c93b5
swy06041 <swy06041@naver.com>
2023-08-21 11:08:16 +0900

    update

--- src/main/java/com/example/appec2/SampleControlller.java
+++ src/main/java/com/example/appec2/SampleControlller.java
@@ -3,13 +3,11 @@ package com.example.appec2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@ResponseBody
 @RequestMapping("/api")
 public class SampleControlller {
-    @GetMapping("/asd")
+    @GetMapping("/nice")
     public String asd() {
         return "nice";
     }
--- src/main/java/com/example/appec2/TestController.java
+++ src/main/java/com/example/appec2/TestController.java
@@ -2,14 +2,21 @@ package com.example.appec2;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@ResponseBody
+@RequestMapping("/api2")
 public class TestController {
 
     @GetMapping("/getArr")
     public String[] getArr() {
         return new String[]{"AAA", "BBB", "CCC"};
     }
+
+//    @GetMapping("/nice")
+//    public String asd() {
+//        return "nice";
+//    }
 }
